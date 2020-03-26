c375d22fe55f990cf698038ba2ece496d27299c2
Kurt Kolbye <kmkolbye@gmail.com>
2020-03-26 18:34:37 -0500

    Added support for making an Ajax POST to the Api and create a new To Do item.

--- TodoWebsite/scripts/script.js
+++ TodoWebsite/scripts/script.js
@@ -7,9 +7,35 @@ $(document).ready(function() {
     
         // Operations
         self.addTodo = function() {
-            self.todoItems.push(new Todo({ name: this.newTodoText() }));
-            // TODO: Make a POST to the server to add the To do
-            // Q: Post entire todoItems JSON or just the one? Probably just the one
+            
+            var newTodo = new Todo({ 
+                name: this.newTodoText(),
+                isComplete: false
+            });
+
+            // Add new Todo to the list of items
+            self.todoItems.push(newTodo);
+
+            var newTodoJSON = ko.toJSON(newTodo);
+
+            $.ajax({
+                type: 'POST',
+                //TODO: Swap url for relative url when hosted on same domain
+                //url: '/api/TodoItems',
+                url: 'https://localhost:44378/api/TodoItems',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: newTodoJSON,
+                success: function (data) {
+                    window.console.log('AJAX POST Success: ' + JSON.stringify(data));
+                },
+                error: function (request, error) {
+                    debugger;
+                    alert('Error got hit on POST.');
+                    window.console.log('Error Request: ' + JSON.stringify(request));
+                    window.console.log('Error: ' + error);
+                }
+            });
         };
         self.removeTodo = function(todoItem) {
             self.todoItems.remove(todoItem);
@@ -24,7 +50,7 @@ $(document).ready(function() {
             url: 'https://localhost:44378/api/TodoItems',
             dataType: 'json',
             success: function (data) {
-                window.console.log('AJAX Success: ' + JSON.stringify(data));
+                window.console.log('AJAX GET Success: ' + JSON.stringify(data));
                 // Convert the data into Todo objects
                 var mappedTodos = $.map(data, function(item) {
                     return new Todo(item);
@@ -33,7 +59,7 @@ $(document).ready(function() {
                 self.todoItems(mappedTodos);
             },
             error: function (request, error) {
-                alert('Error got hit.');
+                alert('Error got hit on GET.');
                 window.console.log('Error Request: ' + JSON.stringify(request));
                 window.console.log('Error: ' + error);
             }
